9cc94bf04e874eeb9e7b1017c2dd1d280db69ba2
Massimo Bonanni <mabonann@microsoft.com>
2021-10-31 18:24:45 +0100

    Add main projects for the solution

---
+++ SCSC.ElfSimulator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SCSC.ElfSimulator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
