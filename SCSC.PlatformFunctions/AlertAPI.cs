de6dd14d4d92d7f0aec4f15e0fad61a92063809a
Massimo Bonanni <mabonann@microsoft.com>
2021-11-13 09:01:14 +0100

    Add GetAlert API

--- SCSC.PlatformFunctions/AlertAPI.cs
+++ SCSC.PlatformFunctions/AlertAPI.cs
@@ -28,14 +28,13 @@ namespace SCSC.PlatformFunctions
         }
 
         //  API to implements
-        //      api/alerts                  --> [GET] retrieve all the alert running
         //      api/alerts/{alertId}        --> [GET] retrieve a specific alert
         //      api/alerts/{alertId}/cancel --> [POST] cancel an alert
         //      api/alerts                  --> [POST] create an alert
 
         #region Open API Definition
         [OpenApiOperation(operationId: "getalerts",
-            Summary = "Get the list of alerts.",
+            Summary = "Get the list of alerts in running state.",
             Visibility = OpenApiVisibilityType.Important)]
         [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
         [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK,
@@ -72,6 +71,36 @@ namespace SCSC.PlatformFunctions
             } while (noFilter.ContinuationToken != null);
 
             return new OkObjectResult(result);
+        }
+
+        #region Open API Definition
+        [OpenApiOperation(operationId: "getalert",
+            Summary = "Get the informatiojn about one specific alert.",
+            Visibility = OpenApiVisibilityType.Important)]
+        [OpenApiSecurity("function_key", SecuritySchemeType.ApiKey, Name = "code", In = OpenApiSecurityLocationType.Query)]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.OK,
+            contentType: "json",
+            bodyType: typeof(AlertInfoModel),
+            Summary = "The information of the alert")]
+        [OpenApiResponseWithBody(statusCode: HttpStatusCode.NotFound,
+            contentType: "json",
+            bodyType: typeof(string),
+            Summary = "If the alert doesn't exist")]
+        #endregion Open API Definition
+        [FunctionName(nameof(GetAlert))]
+        public async Task<IActionResult> GetAlert(
+           [HttpTrigger(AuthorizationLevel.Function, "get", Route = "alerts/{alertId}")] HttpRequest req,
+           string alertId,
+           [DurableClient] IDurableOrchestrationClient client,
+           ILogger logger)
+        {
+            var orchestrator = await client.GetStatusAsync(alertId);
+            if (orchestrator != null)
+            {
+                return new OkObjectResult(orchestrator.ToAlertInfoModel());
+            }
+
+            return new NotFoundObjectResult("The alert doesn't exist");
         }
     }
 }
